0b1e5dd73838813643d11f4c90adf6443ce925b9
Rafal Lagowski <lagowski@pro4.es>
2018-06-10 01:38:58 +0200

    Komunikacja

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            age: props.initialAge,
+            status: 0
+        };
+        setTimeout(() => {
+            this.setState({
+                status: 1
+            });
+        },3000);
+    }
+
+    onMakeOlder() {
+        this.setState({
+            age: this.state.age +3
+        })
+    }
+
     render () {
         return (
             <div>
-                <h1>HOME COMPONENT</h1>
+                <h1>COMPONENT</h1>
+                <p>Your Name is {this.props.name}, your age is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
+                <p>User Object => Name: {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                    </ul>
+                </div>
+                <hr />
+                {this.props.children}
+                <hr />
+                <button onClick={()=> this.onMakeOlder()} className="btn btn-primary" >Make Older </button>
+                <hr />
+                <button onClick={this.props.greet} className="btn btn-primary" >Greet u Parenta!</button>
             </div>
         )
     }
+}
 
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    user: PropTypes.object,
+    initialAge: PropTypes.number,
+    children: PropTypes.element.isRequired
 }
--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,16 @@ import { Header} from './components/Header'
 import { Home} from './components/Home'
 
 class App extends React.Component {
-
+    onGreet() {
+        alert("Hello");
+    }
 
 
     render() {
+        var user = {
+            name: "Anna",
+            hobbies: ["Sports", "ULM"]
+        };
         return (
             <div className="container">
                 <div className="row">
@@ -17,7 +23,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home />
+                        <Home name={"Max"} initialAge={27} user={user} greet={this.onGreet}>
+                            <p>This is paragraph - CHILD</p>
+                        </Home>
                     </div>
                 </div>
             </div>
